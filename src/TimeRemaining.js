883d1b607a73f8061e0aef0cbd6e073109707dfd
Matt Raines <mraines4@gmail.com>
2019-04-26 13:33:22 -0400

    adds sweet explosion gif

--- src/TimeRemaining.js
+++ src/TimeRemaining.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import explosion from './explosion.gif'
 
 function TimeRemaining({time, completion}) {
     return (
         <div>
-            {!completion ? <h1>{time}</h1> : <h1>All done</h1>}
+            {!completion ? <h1>{time}</h1> : <img src={explosion}></img>}
         </div>
     )
 }
